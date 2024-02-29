079f9becab389ee506d8f718d473c58ee0e13e18
Stephan Raabe <mail@stephan-raabe.de>
2024-02-29 17:18:47 +0100

    Updates

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 cd ..
 ARCH=x86_64 appimagetool ml4w-welcome
+echo ":: AppImage created"
 cp ML4W_Welcome-x86_64.AppImage ~/dotfiles-versions/dotfiles/apps/
+echo ":: AppImage copied to ~/dotfiles-versions/dotfiles/apps/"
